569d94b6133e3585eae4a0dba8b2a4413bb066ae
prathapbr1993 <82834371+prathapbr1993@users.noreply.github.com>
2021-11-27 07:19:11 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,36 @@ import Pricing from "./components/Pricing";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Testimonial from "./components/Testimonial";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Header />
-      <Service />
-      <Pricing />
-      <Testimonial />
-      <Contact />
-      <Footer />
+      <Router>
+        <Route path="/">
+          {" "}
+          <Header />
+        </Route>
+        <Route path="/">
+          {" "}
+          <Service />
+        </Route>
+        <Route path="/">
+          {" "}
+          <Pricing />
+        </Route>
+        <Route path="/">
+          {" "}
+          <Testimonial />
+        </Route>
+        <Route path="/">
+          {" "}
+          <Contact />
+        </Route>
+        <Route path="/">
+          {" "}
+          <Footer />
+        </Route>
+      </Router>
     </div>
   );
 }
